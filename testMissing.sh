b99340c78f999b8e0d4af8212b0626de5e60d4eb
dev7ba <dev7ba@protonmail.com>
2022-02-23 17:36:51 +0100

    new test for missing transactions

--- testMissing.sh
+++ testMissing.sh
@@ -23,21 +23,19 @@ read -N 1
 echo "Alice Send to Bob, 1 tx in all mempools since we are connected"
 ./alicest.sh bob
 echo "Wait tx to propagate to other nodes"
-sleep 10s
-echo "Disconnect all nodes to simulate chain reorg."
+sleep 5s
+echo "Disconnect all nodes to simulate disconnection."
 ./disconnect.sh alice bob
 ./disconnect.sh alice carl
 ./disconnect.sh bob carl
 echo "Alice generates 10 tx when disconnected"
 for n in {1..10}; do ./alicest.sh bob; done
-echo "Alice generate 1 block containing 11 transactions"
-./gbalice.sh
-echo "Alice generate another transaction for Bob."
-./alicest.sh bob
-echo "Alice generate another block containing 1 transaction"
-./gbalice.sh
-echo "At this point Alice has generated two blocks and its mempool is empty." 
+echo "At this point Alice has 10 tx for herself" 
 echo "Bob and Carl are oblivious of this since they are disconnected."
+echo "We connect nodes again to see the consecuences."
+./connect.sh alice bob
+./connect.sh alice carl
+./connect.sh bob carl
 echo "Bob mines a block containing 1 transaction (the one sent by alice while connected)"
 ./gbbob.sh
 echo "Bob create 2 transactions to Alice."
@@ -48,14 +46,8 @@ echo "Bob create 3 transactions to Alice."
 for n in {1..3}; do ./bobst.sh alice; done
 echo "Bob mines a block containing last 3 txs."
 ./gbbob.sh
-echo "At this point both alice and bob mempool is empty, but Bob has mined 3 blocks and Alice only 2."
-echo "We connect nodes again to see the chain reorg."
-./connect.sh alice bob
-./connect.sh alice carl
-./connect.sh bob carl
-echo "Wait for reorg in nodes."
-sleep 1s
-echo "Alice overrides its two blocks and all transactions oblivious to other nodes (11) are now in it's mempool."
-./calice.sh getmempoolinfo
-echo "Bob has its mempool empty since Alice does not resend transactions from overrided blocks."
-./cbob.sh getmempoolinfo
+echo "Bob create 4 transactions to Alice."
+for n in {1..4}; do ./bobst.sh alice; done
+echo "Bob mines a block containing last 4 txs."
+./gbbob.sh
+echo "At this point there must be missing txs."
