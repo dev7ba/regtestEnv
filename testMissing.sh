9f60ba367a0fa57f62834bdf393b5febe61e5f38
dev7ba <dev7ba@protonmail.com>
2022-05-25 11:34:45 +0200

    Added testMissing for checking missing and ignored transactions

--- testMissing.sh
+++ testMissing.sh
@@ -5,49 +5,34 @@ echo "Start with clean environment"
 ./rmAllWithWallets.sh
 echo "Start bitoind instances for Alice, Bob and Carl"
 ./upAll.sh
-sleep 5s
+echo "Waiting for all nodes to start"
+sleep 10s
 echo "Create wallets for every node."
 ./cwAll.sh
 echo "Connections"
-./connect.sh alice bob
 ./connect.sh alice carl
-./connect.sh bob carl
+./connect.sh alice bob 
 echo "Alice generates 200 blocks."
 ./gbalice.sh 200
 echo "Bob generates 200 blocks."
 ./gbbob.sh 200
+echo "Carl generates 200 blocks."
+./gbcarl.sh 200
 echo "Create new address for each node"
 ./naAll.sh
 echo "Press key when zmq-listening program is up and connected."
 read -N 1
-echo "Alice Send to Bob, 1 tx in all mempools since we are connected"
-./alicest.sh bob
-echo "Wait tx to propagate to other nodes"
-sleep 5s
-echo "Disconnect all nodes to simulate disconnection."
-./disconnect.sh alice bob
-./disconnect.sh alice carl
-./disconnect.sh bob carl
-echo "Alice generates 10 tx when disconnected"
-for n in {1..10}; do ./alicest.sh bob; done
-echo "At this point Alice has 10 tx for herself" 
-echo "Bob and Carl are oblivious of this since they are disconnected."
-echo "We connect nodes again to see the consecuences."
-./connect.sh alice bob
-./connect.sh alice carl
-./connect.sh bob carl
-echo "Bob mines a block containing 1 transaction (the one sent by alice while connected)"
-./gbbob.sh
-echo "Bob create 2 transactions to Alice."
-for n in {1..2}; do ./bobst.sh alice; done
-echo "Bob mines a block containing last 2 txs."
-./gbbob.sh
-echo "Bob create 3 transactions to Alice."
-for n in {1..3}; do ./bobst.sh alice; done
-echo "Bob mines a block containing last 3 txs."
-./gbbob.sh
-echo "Bob create 4 transactions to Alice."
-for n in {1..4}; do ./bobst.sh alice; done
-echo "Bob mines a block containing last 4 txs."
-./gbbob.sh
-echo "At this point there must be missing txs."
+echo "Disconnecting bob from carl since it is not needed in test."
+./disconnect.sh alice bob 
+
+echo "Iterates disconnection, generation of transaction, then block and reconnection to generate ignored txs..."
+for n in {1..10} 
+do
+  echo "iteration..."
+  ./disconnect.sh alice carl
+  ./alicest.sh carl
+  ./gbcarl.sh
+  ./connect.sh alice carl
+  sleep 1s
+done
+
